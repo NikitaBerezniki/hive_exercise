eef73bf249b74f500a440efa3468fe364a966eb6
Nikita Solonitsyn <galaxyprise@gmail.com>
2022-04-20 14:30:52 +0500

    master

--- lib/model.dart
+++ lib/model.dart
@@ -20,26 +20,64 @@ class Model {
 
   void userAdapterFunc() async {
     if (!Hive.isAdapterRegistered(0)) Hive.registerAdapter(UserAdapter());
-    var box = await Hive.openBox<User>('user');
-    final user = User('Ivan', 54);
-    await box.add(user);
-    final userFromBox = box.getAt(0);
+    if (!Hive.isAdapterRegistered(1)) Hive.registerAdapter(PetAdapter());
+
+    var box_pet = await Hive.openBox<Pet>('pet');
+    final pet = Pet('Musia', 3);
+    await box_pet.add(pet);
+    final pet2 = Pet('Zevs', 4);
+    await box_pet.add(pet2);
+
+    // final petFromBox = box_pet.getAt(0);
+
+    var box_user = await Hive.openBox<User>('user');
+    final user =
+        User('Ivan', 54, pets: HiveList(box_pet, objects: [pet, pet2]));
+    await box_user.add(user);
+    final userFromBox = box_user.getAt(0);
+
     print(userFromBox);
+    userFromBox?.pets?.forEach((element) {
+      print(element.name);
+      box_pet.clear();
+      box_pet.compact();
+      // if (box_pet.isOpen) box_pet.close();
+      box_user.clear();
+      box_user.compact();
+      // if (box_user.isOpen) box_user.close();
+    });
   }
 }
+
 // for generate
 @HiveType(typeId: 0)
-class User {
+class User extends HiveObject {
   @HiveField(0)
   String name;
   @HiveField(1)
   int age;
-  User(this.name, this.age);
+  @HiveField(2)
+  HiveList<Pet>? pets;
+
+  User(this.name, this.age, {this.pets});
 
   @override
-  String toString() => '$name $age';
+  String toString() => 'user: $name $age $pets';
 }
 
+@HiveType(typeId: 1)
+class Pet extends HiveObject {
+  @HiveField(0)
+  String name;
+  @HiveField(1)
+  int age;
+  @HiveField(2)
+  HiveList<Pet>? pets;
+
+  Pet(this.name, this.age);
+  @override
+  String toString() => 'pet: $name $age';
+}
 // class User {
 //   String name;
 //   int age;
